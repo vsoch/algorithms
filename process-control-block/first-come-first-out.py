b9185949dd38e91583763e4b1779d88c54e1301f
Vanessa Sochat <vsochat@stanford.edu>
2018-05-06 12:12:35 -0400

    tweaking header-block

--- process-control-block/first-come-first-out.py
+++ process-control-block/first-come-first-out.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 # 1. First Come First Out
-# Here we do everything in serial, no aync!
+# Here we do everything in serial, no async!
+# Note that we are just *calculating* the times and not just enduring them
 
-# 1-  Input the processes along with their burst time (bt).
-# 2-  Find waiting time (wt) for all processes.
-# 3-  As first process that comes need not to wait so 
-#    waiting time for process 1 will be 0 i.e. wt[0] = 0.
-# 4-  Find waiting time for all other processes i.e. for
-#     process i -> 
-#       wt[i] = bt[i-1] + wt[i-1] .
-# 5-  Find turnaround time = waiting_time + burst_time 
-#    for all processes.
-# 6-  Find average waiting time = 
-#                  total_waiting_time / no_of_processes.
-# 7-  Similarly, find average turnaround time = 
-#                 total_turn_around_time / no_of_processes.
+# 1-Input the processes along with their burst times
+# 2-Find waiting time (wt) for all processes.
+# 3-As first process that comes need not to wait so 
+#   waiting time for process 1 will be 0 i.e. wt[0] = 0.
+# 4-Find waiting time for all other processes i.e. for
+#   process i ->  wt[i] = bt[i-1] + wt[i-1] .
+# 5-Find turnaround time = waiting_time + burst_time 
+#   for all processes.
+# 6-Find average waiting time = 
+#   total_waiting_time / no_of_processes.
+# 7-Similarly, find average turnaround time = 
+#   total_turn_around_time / no_of_processes.
 
 
 class Process(object):
