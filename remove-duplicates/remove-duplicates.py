3d0025ccd5bb8ce6070391afb0722c89f3cf45c3
vsoch <vsochat@stanford.edu>
2020-10-10 14:39:36 -0600

    adding remove duplicates exercise
    Signed-off-by: vsoch <vsochat@stanford.edu>

---
+++ remove-duplicates/remove-duplicates.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        # Edge case, return 0 if there is no array
+        if not nums:
+            return 0
+    
+        # Since the array is sorted, we can start at the end
+        # Keep an index to our current element
+        idx = len(nums) -1
+    
+        # Keep going until index is second element
+        while idx > 0:
+            current = nums[idx]
+            previous = nums[idx-1]
+            # If the current is == the previous, pop off the current
+            if current == previous:
+                nums.pop(idx)
+            # Update the index to be one in
+            idx -=1
+            
+        return len(nums)
